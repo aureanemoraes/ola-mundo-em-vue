34731e460bf4f4945b51ca72bc468b7b217c204e
Aureane Moraes <aureane.moraes@outlook.com>
2022-07-22 14:58:15 -0300

    ola mundo em vue

---
+++ src/components/OlaMundo.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Olá, Mundo!</h1>
+    <h2>Primeiro projeto em vue 3 \o/</h2>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: "OlaMundo"
+    });
+</script>
+
+<style>
+    h1 {
+        color: green;
+    }
+
+    h2 {
+        color: blue;
+    }
+</style>
